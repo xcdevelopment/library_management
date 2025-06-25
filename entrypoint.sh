9b5c05bd93a34212751cc2195c258c0f0cb24d52
xcdevelopment <Development@xcap.co.jp>
2025-06-25 13:31:38 +0900

    feat: Modify code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+# データベースが起動するまで待機
+echo "Waiting for database..."
+# 'db' は docker-compose.yml で定義されたMySQLサービスのホスト名
+# '3306' はMySQLの標準ポート
+while ! nc -z db 3306; do
+  sleep 0.1
+done
+echo "Database started"
 
-# データベースのマイグレーションを実行
+# データベースマイグレーションを実行
+# アプリケーションによっては 'flask db upgrade' の前に 'flask db init' や 'flask db migrate' が必要かもしれません
 echo "Running database migrations..."
 flask db upgrade
 
-# 元のCMDを実行
-exec "$@" 
+# アプリケーションを起動
+exec "$@"
